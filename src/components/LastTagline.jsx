3952d715b942f4e66fa5fac48297a15e60458d5b
medakshchoudhary <ch.daksh01@gmail.com>
2024-07-24 17:01:17 +0530

    fixed path

--- src/components/LastTagline.jsx
+++ src/components/LastTagline.jsx
@@ -4,7 +4,7 @@ import gsap from 'gsap'
 
 function LastTagline() {
     useEffect(()=>{
-        gsap.from(".taglinelast .colon1",{
+        gsap.from(".colon1",{
             y:-70,
             x:-70,
             scrollTrigger:{
@@ -30,9 +30,9 @@ function LastTagline() {
     }, []);
   return (
     <div className="taglinelast w-full h-[100vh] relative overflow-hidden bg-black flex items-center justify-center">
-        <img className="colon1 w-16 absolute top-[20%] left-[10%]" src="/public/svgs/quote-left.svg" alt="" />
+        <img className="colon1 w-16 absolute top-[20%] left-[10%]" src="/svgs/quote-left.svg" alt="" />
         <h1 className="text-[10vw] font-extrabold uppercase text-[#DA0007] flex flex-col items-center justify-center leading-none"><span>Wining isn't</span> <span> for everyone</span></h1>
-        <img className="colon2 w-16 absolute bottom-[20%] right-[5%]" src="/public/svgs/quote-right.svg" alt="" />
+        <img className="colon2 w-16 absolute bottom-[20%] right-[5%]" src="/svgs/quote-right.svg" alt="" />
     </div>
   )
 }
