dc4fe87d653c2169ca6354f226a2e679f5ba63b8
medakshchoudhary <ch.daksh01@gmail.com>
2024-07-26 00:41:44 +0530

    showcase is done

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -25,21 +25,21 @@ function Showcase({ selectedShoe }) {
         ]);
     })
     return (
-        <div className="showcase w-full h-[55vh] px-12 flex mt-7">
-            <div className="nike w-[56vw] h-full overflow-hidden">
-                <h1 className="text-[23vw] leading-none tracking-tighter">NIKE</h1>
+        <div className="showcase w-full h-[55vh] md:px-12 md:flex mt-7">
+            <div className="nike md:w-[56vw] w-full h-fit overflow-hidden">
+                <h1 className="md:text-[23vw] text-[46vw] leading-none tracking-tighter">NIKE</h1>
             </div>
-            <div className="shoe-display w-1/2 flex flex-col items-center justify-center mr-6">
-                <div className="image h-1/2 w-full flex justify-end">
+            <div className="shoe-display md:w-fit w-full flex flex-col items-center justify-center mr-6">
+                <div className="image h-1/2 md:w-full w-[80vw] flex justify-end">
                     <img 
-                        className="max-h-full max-w-full object-contain" 
+                        className="md:max-h-full w-full object-contain" 
                         src={selectedShoe.src} 
                         alt={selectedShoe.name} 
                     />
                 </div>
-                <div className="shoe-details h-1/3 w-full flex flex-col items-end justify-center">
-                    <h1 className="shoe-name text-6xl">{selectedShoe.name}</h1>
-                    <h1 className="shoe-price text-3xl">{selectedShoe.price}</h1>
+                <div className="shoe-details md:p-0 px-10 h-1/3 w-full flex flex-col items-end justify-center">
+                    <h1 className="shoe-name text-4xl md:text-6xl">{selectedShoe.name}</h1>
+                    <h1 className="shoe-price text-2xl md:text-3xl">{selectedShoe.price}</h1>
                 </div>
             </div>
         </div>
