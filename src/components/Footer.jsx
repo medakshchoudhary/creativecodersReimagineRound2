d79f67036945a7a7e426633cf1fb92a67d21f315
medakshchoudhary <ch.daksh01@gmail.com>
2024-07-25 16:47:39 +0530

    made a string effect

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import gsap from 'gsap'
 
 function Footer() {
-  return (
-    <div className="footer w-full h-[100vh] bg-black flex felx-col">
-        <div className="string">
+    const [path, setPath] = useState("M 0 100 Q 760 100 1520 100")
+    const finalPath = "M 0 100 Q 760 100 1520 100"
 
-        </div>
-        <div className="creativecoders">
+    useEffect(() => {
+        const string = document.querySelector('.string')
 
+        string.addEventListener("mousemove", function(dets) {
+            const newPath = `M 0 100 Q ${dets.clientX} ${dets.clientY} 1520 100`
+            setPath(newPath)
+            gsap.to("svg path", {
+                attr: { d: newPath },
+                duration: 0.3,
+                ease: "power3.out",
+            })
+        })
+
+        string.addEventListener("mouseleave", function() {
+            setPath(finalPath)
+            gsap.to("svg path", {
+                attr: { d: finalPath },
+                duration: 1.5,
+                ease: "elastic.out(1,0.2)",
+            })
+        })
+    }, [])
+
+    return (
+        <div className="footer w-full h-[100vh] bg-black flex felx-col">
+            <div className="string bg-black w-full h-[200px] flex items-center justify-center">
+                <svg width="100%" height="200">
+                    <path d={path} stroke="white" strokeWidth="2" fill="transparent" />
+                </svg>
+            </div>
+            <div className="creativecoders">
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Footer
